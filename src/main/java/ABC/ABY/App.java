810e29a5a70a858af17407ef8610494498cce17d
rahul <rahulpawar2353@gmail.com>
2022-07-07 11:15:50 +0530

    hi

---
+++ src/main/java/ABC/ABY/App.java
@@ -0,0 +1,7 @@
+package ABC.ABY;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/ABC/ABY/AppTest.java
@@ -0,0 +1,14 @@
+package ABC.ABY;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+    System.out.println("Hello java");
+  }
+}
